c567882473d6161cf323eeac4309974b128d977d
luis_sombras <luismsombrei@gmail.com>
2017-01-20 23:33:52 +0000

    First commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float cam_velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+        //camera.transform.
+	}
+}
---
+++ Assets/WavesController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WavesController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject[] movables;
+            movables = GameObject.FindGameObjectsWithTag("wave_movable");
+
+            for(int i = 0; i < movables.Length; i++)
+            {
+                Debug.Log("Created Wave " + i + " " + movables[i].GetType());
+            }
+        }
+	}
+}
