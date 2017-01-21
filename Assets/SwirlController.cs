d56eeeed0ea3140ccc5aa6c9a0dd4dad01cba06e
luis_sombras <luismsombrei@gmail.com>
2017-01-21 17:14:33 +0000

    luis commit - started working on swirl

---
+++ Assets/SwirlController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwirlController : MonoBehaviour {
+
+    private Rigidbody2D player_rb;
+    private Vector2 swirl_pos;
+    private Vector2 frog_pos;
+    private Vector2 radial;
+    public float cntr_force;
+    public float perp_force;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            player_rb = coll.gameObject.GetComponent<Rigidbody2D>();
+            frog_pos = new Vector2(coll.gameObject.transform.position.x, coll.gameObject.transform.position.y);
+            swirl_pos = new Vector2(transform.position.x, transform.position.y);
+
+            //central force
+            radial = swirl_pos - frog_pos;
+            //radial
+
+
+            //player_rb.AddForce
+        }
+        
+    }
+}
