3d5d4a531617267b2a23e06083761504d19b8824
Jaime Hernández <jaimevazz@gmail.com>
2017-01-22 12:18:19 +0000

    Audio stuff. Scene loops
    + Added AudioController, AudioManager script and scene loops.
    - Modified GameManager to Singleton
    TODO: Fix Screens management (Canvas) in game scene.
    TODO: Add sound FX

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : Singleton<GameManager> {
 
 	[SerializeField] private GameObject wavesInput;
+	[SerializeField] private AudioClip[] sceneLoops;
 
 	private bool paused = false;
 	// NOTIFIER
 	private Notifier notifier;
 
+	public bool iAmFirst;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(Instance);
+
+		GameManager[] gameManagers = FindObjectsOfType(typeof(GameManager)) as GameManager[];
+
+		if(gameManagers.Length > 1)
+		{
+			for(int i = 0; i < gameManagers.Length; i++)
+			{
+				if(!gameManagers[i].iAmFirst)
+				{
+					DestroyImmediate(gameManagers[i].gameObject);
+				}
+			}
+		}
+		else
+		{
+			iAmFirst = true;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 		// NOTIFIER
 		notifier = new Notifier ();
+		//StartAudioLoop ();
+	}
+
+	void OnEnable() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnDisable() {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		//do stuff
+		Debug.Log("Level Loaded");
+		Debug.Log(scene.name);
+		Debug.Log(mode);
+		StartAudioLoop (scene.buildIndex);
+	}
+
+	private void StartAudioLoop (int sceneIndex) {
+		//int scene = SceneManager.GetActiveScene ().buildIndex;
+		AudioManager.Instance.PlayLoop2D ("Scene_"+sceneIndex.ToString(), sceneLoops [sceneIndex]);
 	}
 
 	public void OnPause () {
